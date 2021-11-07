6aa17b0a9bd0e1632da24cd2f4cc0c08b013a739
Justin Molina <jmac@justins-mbp.myfiosgateway.com>
2021-11-07 13:45:49 -0500

    Hullthread API

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ class App extends Component {
       foo: "bar",
       resumeData: {}
     };
-
-    ReactGA.initialize("UA-110570651-1");
-    ReactGA.pageview(window.location.pathname);
   }
 
   getResumeData() {
